7e00bec06542a1be645283fce9a127f82b946cdf
Spid3rrr <bra.ami20@hotmail.fr>
2021-02-15 21:51:13 +0100

    deez commits

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
 import time
 import requests
 import os
+import pandas as pd
+import ast
 from bs4 import BeautifulSoup
-
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -16,18 +19,9 @@ def getdata():
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://lolesports.com/schedule?leagues=lec")
-
-
-
-
-
-
-
     html = driver.page_source
-
     soup = BeautifulSoup(html,features="html.parser")
     results = soup.find_all('div', class_ = 'team team1')
-
     print('result : \n')
     for result in results:
         teamname = result.find('span',class_='name')
@@ -35,9 +29,18 @@ def getdata():
             continue
         print(teamname.text.strip())
         print()
-
-
     driver.quit()
+class teams(Resource):
+    #methods go here
+    def get(self):
+        return {'test value':' noice '},200
+    pass
+
+
+
 #our main function
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(teams,'/teams')
 if __name__ == '__main__':
-    getdata() #call our counting method
+    getdata() #get data
